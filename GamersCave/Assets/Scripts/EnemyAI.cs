24e68d61207a8f109d7c693117eed5acc1ebc5a1
Vena Ketpongsuda <>
2020-02-13 21:29:22 -0800

    GamersCave: Player & Enemy Movements

---
+++ GamersCave/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform playerTarget;
+    [SerializeField] float enemyPOV = 5f;
+
+    NavMeshAgent meshAgent;
+    float playerDistance = Mathf.Infinity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyChase();
+    }
+
+    public void enemyChase()
+    {
+        playerDistance = Vector3.Distance(playerTarget.position, transform.position);
+
+        if (playerDistance <= enemyPOV)
+        {
+            meshAgent.SetDestination(playerTarget.position);
+        }
+    }
+}
